a46288f76cbec634be314d972a1e4ccb6c8314fd
Paweł Pindel <pawel.pindel@netsharpdev.com>
2020-03-07 11:30:15 +0100

    Implementation of exception handling middleware

---
+++ AppBuilderExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Netsharpdev.ExceptionHandlingMiddleware
+{
+    public static class AppBuilderExtension
+    {
+        public static IApplicationBuilder UseExceptionHandlingMiddleware(this IApplicationBuilder applicationBuilder) => UseExceptionHandlingMiddleware(null);
+        public static IApplicationBuilder UseExceptionHandlingMiddleware(this IApplicationBuilder applicationBuilder, string errorMessage)
+        {
+            applicationBuilder.UseMiddleware<ExceptionHandlingMiddleware>(errorMessage);
+            return applicationBuilder;
+        }
+    }
+}
---
+++ ExceptionHandlingMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Dynamic;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace Netsharpdev.ExceptionHandlingMiddleware
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly string _errorMessage;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next, string errorMessage)
+        {
+            _next = next;
+            _errorMessage = errorMessage ?? "We are sorry, but something went wrong.";
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var code = HttpStatusCode.InternalServerError;
+            dynamic errorObject = new ExpandoObject();
+            errorObject.error = _errorMessage;
+            errorObject.exception = exception;
+
+            string result = JsonConvert.SerializeObject(
+                errorObject,
+                new JsonSerializerSettings {ReferenceLoopHandling = ReferenceLoopHandling.Ignore});
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int) code;
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
